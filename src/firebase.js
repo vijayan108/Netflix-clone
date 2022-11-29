294dd4c427830bee8f2e5b139e68a35f28119457
The Coder <70505855+vijayan108@users.noreply.github.com>
2022-11-29 16:07:27 +0000

    changes

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,3 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBL2b9b8SvZ5eAjMNA__RCH2hzsW0O6Hd8",
-  authDomain: "netflix-clone-1b942.firebaseapp.com",
-  projectId: "netflix-clone-1b942",
-  storageBucket: "netflix-clone-1b942.appspot.com",
-  messagingSenderId: "279172810084",
-  appId: "1:279172810084:web:8cb88c3f27aa9d067d2671",
-  measurementId: "G-XWKPYNS406",
+ // Enter your Config details
 };
